1ee42b332a02297db37efbad539c0118d9bfd382
Michael Hudson <mnkotagu@gmail.com>
2023-10-22 11:09:09 +0300

    Implement valuation register fourth step

--- app/src/main/java/com/aru/valuationregister/ValuationRegister/FormStepFour.java
+++ app/src/main/java/com/aru/valuationregister/ValuationRegister/FormStepFour.java
@@ -5,19 +5,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 import com.aru.valuationregister.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 
 public class FormStepFour extends Fragment {
 
     private boolean isComplete;
-
-    private ParentFormWizard parentFormWizard;
-
+    private ParentFormWizard parentActivity;
+    private EditText servicesAmenitiesEditText;
+    private List<String> servicesAmenitiesIds;
+    private List<String> servicesAmenitiesText;
+    private TextInputLayout otherServicesAmenitiesInputLayout;
+    private AutoCompleteTextView howLandWasAcquiredTextView;
+    private TextInputLayout otherWayLandWasAcquiredInputLayout;
+    private EditText whenPlotWasPurchasedEditText;
+    private EditText otherWayLandWasAcquiredEditText;
+    private EditText howMuchWasPaidToAcquireLandEditText;
+    private EditText otherServicesAmenitiesEditText;
     private Intent intent;
 
 
@@ -28,10 +43,7 @@ public class FormStepFour extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.form_step_two, container, false);
-
-        // Get ui form widgets
-
+        View v = inflater.inflate(R.layout.form_step_four, container, false);
 
         //Set the intent used to persist data across fragments
         intent = requireActivity().getIntent();
@@ -42,11 +54,16 @@ public class FormStepFour extends Fragment {
         }
 
         // Get the instance of the Parent form wizard with form building helpers
-        parentFormWizard = (ParentFormWizard) getActivity();
-        assert parentFormWizard != null;
+        parentActivity = (ParentFormWizard) getActivity();
+        assert parentActivity != null;
 
         // Add dropdown lists to the parent activity, these will be used to initialize the widget
         isComplete = false;
+
+        // Initialize UI widgets
+        initializeServicesAmenitiesWidget(v);
+        initializePropertyConditionWidget(v);
+        initializeEditTexts(v);
 
         // Set persisted form data to current UI elements
         displayBoundDataFields();
@@ -56,17 +73,106 @@ public class FormStepFour extends Fragment {
         return v;
     }
 
+    private void initializeServicesAmenitiesWidget(View v) {
+        servicesAmenitiesIds = new ArrayList<>();
+        servicesAmenitiesText = new ArrayList<>();
+        servicesAmenitiesEditText = v.findViewById(R.id.services_amenities_text_view);
+        otherServicesAmenitiesInputLayout = v.
+                findViewById(R.id.other_services_amenities_input_layout);
+
+        servicesAmenitiesEditText.setOnClickListener(v1 ->
+                parentActivity.
+                        getDialogSelector(servicesAmenitiesEditText,
+                                "services-amenities",
+                                R.string.services_amenities_in_plot, (configuration, type) -> {
+                                    if (type.equals("checked")) {
+                                        servicesAmenitiesIds.
+                                                add(configuration.configurationId);
+                                        servicesAmenitiesText.
+                                                add(configuration.description);
+
+                                        if (configuration.description.
+                                                equals("Any Other")) {
+                                            otherServicesAmenitiesInputLayout.
+                                                    setVisibility(View.VISIBLE);
+                                        }
+                                    } else if (type.equals("unchecked")) {
+                                        servicesAmenitiesIds.
+                                                remove(configuration.configurationId);
+                                        servicesAmenitiesText.
+                                                remove(configuration.description);
+
+                                        if (configuration.description.
+                                                equals("Any Other")) {
+                                            otherServicesAmenitiesInputLayout.
+                                                    setVisibility(View.GONE);
+                                        }
+                                    }
+                                    formData.putString("servicesAmenitiesIds",
+                                            android.text.TextUtils.join(",",
+                                                    servicesAmenitiesIds));
+                                    formData.putString("servicesAmenitiesText",
+                                            android.text.TextUtils.join(", ",
+                                                    servicesAmenitiesText));
+                                })
+        );
+    }
+
+    private void initializePropertyConditionWidget(View v) {
+        howLandWasAcquiredTextView = v.findViewById(R.id.how_land_was_acquired_text_view);
+        otherWayLandWasAcquiredInputLayout = v.
+                findViewById(R.id.other_way_land_was_acquired_input_layout);
+        parentActivity.initializeDropdownLists(howLandWasAcquiredTextView,
+                "land-acquisition");
+        howLandWasAcquiredTextView.setOnItemClickListener((parent, view, position, id) -> {
+            parentActivity.getConfigurationItemId("land-acquisition",
+                    parent.getItemAtPosition(position).toString(), (configuration, type) -> {
+                        formData.putString("landAcquisitionId", configuration.configurationId);
+                        formData.putString("landAcquisitionText", configuration.description);
+            });
+
+            if (parent.getItemAtPosition(position).toString().equals("Any Other")) {
+                otherWayLandWasAcquiredInputLayout.setVisibility(View.VISIBLE);
+            } else {
+                otherWayLandWasAcquiredInputLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void initializeEditTexts(View v) {
+        whenPlotWasPurchasedEditText = v.
+                findViewById(R.id.when_plot_was_purchased_edit_text);
+        otherWayLandWasAcquiredEditText = v.
+                findViewById(R.id.other_way_land_was_acquired_edit_text);
+        howMuchWasPaidToAcquireLandEditText = v.
+                findViewById(R.id.how_much_was_paid_to_acquire_land_edit_text);
+        otherServicesAmenitiesEditText = v.
+                findViewById(R.id.other_services_amenities_edit_text);
+    }
+
     /**
      * Called whenever the wizard proceeds to the next step or goes back to the previous step
      */
     private void bindDataFields() {
 
-        isComplete = formData.get("villageStreetId") != null &&
-                formData.get("landPlotId") != null &&
-                formData.get("notableLandmarks") != null &&
-                formData.get("propertyAccessibilityIds") != null;
+        formData.putString("whenPlotWasPurchased", whenPlotWasPurchasedEditText.
+                getText().toString());
+        formData.putString("otherWayLandWasAcquired", otherWayLandWasAcquiredEditText
+                .getText().toString());
+        formData.putString("howMuchWasPaidToAcquireLand", howMuchWasPaidToAcquireLandEditText.
+                getText().toString());
+        formData.putString("otherServicesAmenities", otherServicesAmenitiesEditText.
+                getText().toString());
+
+        isComplete = formData.get("whenPlotWasPurchased") != null &&
+                formData.get("servicesAmenitiesIds") != null &&
+                formData.get("whenPlotWasPurchased") != null &&
+                formData.get("landAcquisitionText") != null;
         intent.putExtras(formData);
+        isComplete = true;
     }
+
+
 
     private void displayBoundDataFields() {
         if (formData != null) {
